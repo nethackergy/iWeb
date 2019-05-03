c81043f86b2c7b75b782f64d686fa15da7732cff
nethackergy <46089005+nethackergy@users.noreply.github.com>
2019-05-03 22:17:57 +0800

    Update jenkinsfile_master.groovy

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -1,6 +1,6 @@
 stage('pull source code') {
     node('master'){
-        git([url: 'git@github.com:princeqjzh/iWeb.git', branch: 'master'])
+        git([url: 'git@github.com:nethackergy/iWeb.git', branch: 'master'])
     }
 }
 
